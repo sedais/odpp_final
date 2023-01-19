f97dd983002afd5270a8990545c55d81a12f5f67
sedais <seda.metin.ismail@gmail.com>
2023-01-19 11:11:07 +0100

    delete test graph

--- visualization/pages/Channel_Statistics.py
+++ visualization/pages/Channel_Statistics.py
@@ -73,16 +73,16 @@ data = load_channels()
 # filtered_df = data[if data['Title'] in list]
 grouped_df = df.groupby('Channel Name')
 
-
-labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-sizes = [15, 30, 45, 10]
-explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-st.pyplot(fig1)
+#
+# labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+# sizes = [15, 30, 45, 10]
+# explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+#
+# fig1, ax1 = plt.subplots()
+# ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+#         shadow=True, startangle=90)
+# ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+#
+# st.pyplot(fig1)
 
 # data = load_channels()
